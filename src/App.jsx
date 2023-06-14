495f921b6cd2cc365e341b57a1f918f87076bfd6
jibola2588 <jibolafawole258@gmail.com>
2023-06-14 15:38:17 +0100

    first cli repository

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Navbar} from './components/Navbar'
+import {Header} from './components/Header'
+import {Footer} from './components/Footer'
+
+export const App = () => { 
+    return (
+        <div>
+            <Navbar />
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
