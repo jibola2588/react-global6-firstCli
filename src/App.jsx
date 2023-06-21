bae442c7ac300676429f6204013559ef9a65caf1
Irene Archibong <127126944+Archibong001@users.noreply.github.com>
2023-06-21 16:51:32 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,14 @@
-import  { Component } from 'react'
-import Button from './components/Button'
+import {Navbar} from './components/Navbar'
+import {Header} from './components/Header'
+import {Footer} from './components/Footer'
 
-// stateful
-export default class App extends Component{ 
-   render(){
-    const age = 34;
-     return(
+export const App = () => { 
+    return (
         <div>
-           <h3>Reuseable components</h3>
-           <p>we like white soup at {age}</p>
-           <div class='flex flex-col gap-2 '>
-              <Button 
-                text = 'click me'
-              />
-              <Button 
-                text = 'apply now'
-              />
-              <Button 
-                text = 'download here'
-              />
-              <Button 
-              text = 'signup'
-              />
-              <Button
-              text = 'signin'
-               />
-           </div>
+            <Navbar />
+            <Header />
+            <Footer />
         </div>
-       
-     )
-   }
+    )
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
